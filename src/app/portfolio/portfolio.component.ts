46dda81335bb536bb43f2323b8fbe806e850ac3f
Mark Stein <m.stein@mosst.de>
2018-08-08 19:04:19 +0200

    add themes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
 
 import { PortfolioService } from '../portfolio.service';
+import { Observable, of, BehaviorSubject, from } from 'rxjs';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements OnInit, OnDestroy {
 
-  constructor(public portfolioService: PortfolioService) { }
+  currentTheme: string = 'nerd';
+  themes: string[] = ['nerd', 'playground'];
+
+
+  constructor(public portfolioService: PortfolioService,
+    private renderer: Renderer2) { 
+      this.changeTheme(this.currentTheme);
+    }
+
+    changeTheme(theme: string){
+      this.renderer.removeClass(document.body,this.currentTheme);
+      this.currentTheme = theme;
+      this.renderer.addClass(document.body,this.currentTheme);
+    }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.renderer.removeClass(document.body,'nerd');
+    this.renderer.removeClass(document.body,'classic');
   }
 
 }
