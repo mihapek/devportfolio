c9088f7186990a34cf8905947e67a714ed4a96a8
Mark Stein <m.stein@mosst.de>
2019-04-25 20:16:33 +0200

    dark is default theme

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -8,7 +8,7 @@ import { PortfolioService } from "src/app/portfolio.service";
   styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  currentTheme = "nerd";
+  currentTheme = "white";
   themes: string[] = ["nerd", "material", "white", "black"];
 
   messages = {};
